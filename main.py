265956c1acf47a1d87bf2847d721ef1c24667468
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-05-19 14:23:16 +0300

    Naively limit search to stock RuuviTag Weather Station beacons

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ bluetooth.start_scan(5)
 
 while bluetooth.isscanning():
     adv = bluetooth.get_adv()
-    if adv:
+    if adv \
+    and adv.adv_type == Bluetooth.CONN_ADV \
+    and adv.addr_type == Bluetooth.RANDOM_ADDR \
+    and adv.data[13:22] == b'\x03ruu.vi/#':
         print(repr(adv))
