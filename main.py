a71894f9c4a91c8e7509d4d03651a6668ada6ef9
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-05-19 14:21:22 +0300

    Find advertising bluetooth devices

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # main.py -- put your code here!
+from network import Bluetooth
+
+bluetooth = Bluetooth()
+
+bluetooth.start_scan(5)
+
+while bluetooth.isscanning():
+    adv = bluetooth.get_adv()
+    if adv:
+        print(repr(adv))
