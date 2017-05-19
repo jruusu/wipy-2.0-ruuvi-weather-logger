f344ed5f64fc974ec1aa016c1f0efcb6e7a24479
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-05-19 14:26:49 +0300

    Extract encoded measurements from adv.data

--- main.py
+++ main.py
@@ -11,4 +11,9 @@ while bluetooth.isscanning():
     and adv.adv_type == Bluetooth.CONN_ADV \
     and adv.addr_type == Bluetooth.RANDOM_ADDR \
     and adv.data[13:22] == b'\x03ruu.vi/#':
-        print(repr(adv))
+        # URL-safe Base64 encoded measurements
+        # See: https://github.com/ruuvi/ruuvi-sensor-protocols
+        encoded_measurements = adv.data[22:] \
+            .decode() \
+            .rstrip('\0')
+        print(encoded_measurements)
