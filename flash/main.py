c6e8789543791730d7529f98b67c230984c4cdd5
Jan Ruusuvuori <jan.ruusuvuori@weave.fi>
2017-05-19 15:04:11 +0300

    Refactor: extract function

--- flash/main.py
+++ flash/main.py
@@ -2,22 +2,24 @@
 import binascii
 from network import Bluetooth
 
-bluetooth = Bluetooth()
+def scan_for_measurements(bluetooth):
+    """Find RuuviTag Weather Station beacons, print measurements"""
+    bluetooth.start_scan(5)
 
-bluetooth.start_scan(5)
+    while bluetooth.isscanning():
+        adv = bluetooth.get_adv()
+        if adv \
+        and adv.adv_type == Bluetooth.CONN_ADV \
+        and adv.addr_type == Bluetooth.RANDOM_ADDR \
+        and adv.data[13:22] == b'\x03ruu.vi/#':
+            # URL-safe Base64 encoded measurements
+            # See: https://github.com/ruuvi/ruuvi-sensor-protocols
+            encoded_measurements = adv.data[22:] \
+                .decode() \
+                .rstrip('\0')
 
-while bluetooth.isscanning():
-    adv = bluetooth.get_adv()
-    if adv \
-    and adv.adv_type == Bluetooth.CONN_ADV \
-    and adv.addr_type == Bluetooth.RANDOM_ADDR \
-    and adv.data[13:22] == b'\x03ruu.vi/#':
-        # URL-safe Base64 encoded measurements
-        # See: https://github.com/ruuvi/ruuvi-sensor-protocols
-        encoded_measurements = adv.data[22:] \
-            .decode() \
-            .rstrip('\0')
+            beacon_mac = binascii.hexlify(adv.mac).decode()
 
-        beacon_mac = binascii.hexlify(adv.mac).decode()
+            print(beacon_mac + ": " + encoded_measurements)
 
-        print(beacon_mac + ": " + encoded_measurements)
+scan_for_measurements(Bluetooth())
